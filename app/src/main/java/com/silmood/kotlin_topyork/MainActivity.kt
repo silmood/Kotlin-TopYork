4399ff4f6debcd973a7946ce0542b7de27512119
Pedro Antonio <petterlx727@gmail.com>
2016-04-12 15:45:01 -0500

    Initialize your project

--- app/src/main/java/com/silmood/kotlin_topyork/MainActivity.kt
+++ app/src/main/java/com/silmood/kotlin_topyork/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.silmood.kotlin_topyork
 
-import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : FragmentContainerActivity() {
+class MainActivity : AppCompatActivity() {
 
-    override fun createFragment(): Fragment {
-        return TopStoriesFragment()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
+
     }
 
-    override fun getLayout(): Int {
-        return R.layout.activity_fragment
-    }
 }
